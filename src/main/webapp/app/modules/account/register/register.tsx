c56cbb4761ff92de9ae20731241827712f97115e
Lucia-jiang <lu.jiang@alumnos.upm.es>
2023-03-14 20:30:14 +0100

    Añadidos algunos campos

--- src/main/webapp/app/modules/account/register/register.tsx
+++ src/main/webapp/app/modules/account/register/register.tsx
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 import PasswordStrengthBar from 'app/shared/layout/password/password-strength-bar';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { handleRegister, reset } from './register.reducer';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export const RegisterPage = () => {
   const [password, setPassword] = useState('');
@@ -23,6 +24,7 @@ export const RegisterPage = () => {
   };
 
   const updatePassword = event => setPassword(event.target.value);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const successMessage = useAppSelector(state => state.register.successMessage);
 
@@ -32,8 +34,13 @@ export const RegisterPage = () => {
     }
   }, [successMessage]);
 
+
   return (
     <div>
+      <Row className="justify-content-center">
+        <ProgressBar animated  now={50} label={"Introducción datos personales"}></ProgressBar>
+      </Row>
+
       <Row className="justify-content-center">
         <Col md="8">
           <h1 id="register-title" data-cy="registerTitle">
@@ -46,7 +53,7 @@ export const RegisterPage = () => {
           <ValidatedForm id="register-form" onSubmit={handleValidSubmit}>
             <ValidatedField
               name="username"
-              label="Usuario"
+              label="Usuario (*)"
               placeholder="Nombre de usuario"
               validate={{
                 required: { value: true, message: 'Su nombre de usuario es obligatorio.' },
@@ -56,8 +63,52 @@ export const RegisterPage = () => {
                 },
                 minLength: { value: 1, message: 'Su nombre de usuario debe tener al menos 1 caracter.' },
                 maxLength: { value: 50, message: 'Su nombre de usuario no puede tener más de 50 caracteres.' },
+                // TODO: verificación no repetición
               }}
               data-cy="username"
+            />
+            <ValidatedField
+              name="name"
+              label="Nombre (*)"
+              placeholder="Nombre"
+              validate={{
+                required: { value: true, message: 'Su nombre es obligatorio.' },
+                pattern: {
+                  value: /^[ÁÉÍÓÚÑÜA-Za-záéíóúñü]+(\s+[ÁÉÍÓÚÑA-Z]?[a-záéíóúñ]+)*$/,
+                  message: 'Su nombre no tiene el formato válido.',
+                },
+                minLength: { value: 1, message: 'Su nombre debe tener al menos 1 caracter.' },
+                maxLength: { value: 50, message: 'Su nombre de usuario no puede tener más de 50 caracteres.' }
+              }}
+              data-cy="name"
+            />
+            <ValidatedField
+              name="surname1"
+              label="Apellido 1"
+              placeholder="Apellido 1"
+              validate={{
+                pattern: {
+                  value: /^[ÁÉÍÓÚÑÜA-Za-záéíóúñü]+(\s+[ÁÉÍÓÚÑA-Z]?[a-záéíóúñ]+)*$/,
+                  message: 'El formato de su apellido no es válido.',
+                },
+                maxLength: { value: 50, message: 'Su apellido no puede tener más de 50 caracteres.' },
+                // TODO: verificación no repetición
+              }}
+              data-cy="surname1"
+            />
+            <ValidatedField
+              name="surname2"
+              label="Apellido 2"
+              placeholder="Apellido 2"
+              validate={{
+                pattern: {
+                  value: /^[ÁÉÍÓÚÑÜA-Za-záéíóúñü]+(\s+[ÁÉÍÓÚÑA-Z]?[a-záéíóúñ]+)*$/,
+                  message: 'El formato de su apellido no es válido.',
+                },
+                maxLength: { value: 50, message: 'Su apellido no puede tener más de 50 caracteres.' },
+                // TODO: verificación no repetición
+              }}
+              data-cy="surname2"
             />
             <ValidatedField
               name="email"
@@ -99,6 +150,39 @@ export const RegisterPage = () => {
               }}
               data-cy="secondPassword"
             />
+
+            <ValidatedField
+              name="introductionText"
+              label="Breve texto de introducción"
+              placeholder="Texto introducción"
+              validate={{
+                maxLength: { value: 2500, message: 'Su texto de introducción no puede tener más de 2500 caracteres' }
+              }}
+              data-cy="secondPassword"
+            />
+
+            {/*TODO: pasar imagen a tamaño correspondiente*/}
+            {selectedImage && (
+              <div>
+                <img
+                  alt="not found"
+                  width={"250px"}
+                  src={URL.createObjectURL(selectedImage)}
+                />
+                <br />
+                <button onClick={() => setSelectedImage(null)}>Quitar</button>
+              </div>
+            )}
+
+            <input
+              type="file"
+              name="myImage"
+              onChange={(event) => {
+                console.log(event.target.files[0]);
+                setSelectedImage(event.target.files[0]);
+              }}
+            />
+
             <Button id="register-submit" color="primary" type="submit" data-cy="submit">
               Crear la cuenta
             </Button>
