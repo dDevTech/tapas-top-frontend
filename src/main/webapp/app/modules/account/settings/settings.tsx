10a064299c0b3f7de412b4702f98c22e3b8dc874
Lucia-jiang <lu.jiang@alumnos.upm.es>
2023-03-15 22:12:11 +0100

    Correcciones testing

--- src/main/webapp/app/modules/account/settings/settings.tsx
+++ src/main/webapp/app/modules/account/settings/settings.tsx
@@ -1,23 +1,24 @@
 import React, {useEffect, useMemo} from 'react';
-import { Button, Col, Row } from 'reactstrap';
-import { ValidatedField, ValidatedForm, isEmail } from 'react-jhipster';
-import { toast } from 'react-toastify';
+import {Button, Col, Row} from 'reactstrap';
+import {ValidatedField, ValidatedForm, isEmail} from 'react-jhipster';
+import {toast} from 'react-toastify';
 
-import { useAppDispatch, useAppSelector } from 'app/config/store';
-import { getSession } from 'app/shared/reducers/authentication';
-import { saveAccountSettings, reset } from './settings.reducer';
+import {useAppDispatch, useAppSelector} from 'app/config/store';
+import {getSession} from 'app/shared/reducers/authentication';
+import {saveAccountSettings, reset} from './settings.reducer';
 import countryList from 'react-select-country-list'
 import Select from 'react-select'
+
 export const SettingsPage = () => {
   const dispatch = useAppDispatch();
   const account = useAppSelector(state => state.authentication.account);
   const successMessage = useAppSelector(state => state.settings.successMessage);
 
   const options = [
-    { value: 'none', label: 'Not selected' },
-    { value: 'male', label: 'Male' },
-    { value: 'female', label: 'Female' },
-    { value: 'notsay', label: 'Rather not say' }
+    {value: 'none', label: 'No seleccionar'},
+    {value: 'male', label: 'Hombre'},
+    {value: 'female', label: 'Mujer'},
+    {value: 'notsay', label: 'Prefiero no indicarlo'}
   ]
   const optionsCountry = useMemo(() => countryList().getData(), [])
   useEffect(() => {
@@ -56,13 +57,13 @@ export const SettingsPage = () => {
               id="username"
               placeholder="Su usuario"
               validate={{
-                required: { value: true, message: 'Su nombre de usuario es obligatorio.' },
+                required: {value: true, message: 'Su nombre de usuario es obligatorio.'},
                 pattern: {
                   value: /^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$/,
                   message: 'Su nombre de usuario no es válido.',
                 },
-                minLength: { value: 1, message: 'Su nombre de usuario debe tener al menos 1 caracter.' },
-                maxLength: { value: 50, message: 'Su nombre de usuario no puede tener más de 50 caracteres.' },
+                minLength: {value: 1, message: 'Su nombre de usuario debe tener al menos 1 caracter.'},
+                maxLength: {value: 50, message: 'Su nombre de usuario no puede tener más de 50 caracteres.'},
               }}
               data-cy="username"
             />
@@ -72,35 +73,36 @@ export const SettingsPage = () => {
               id="firstName"
               placeholder="Su nombre"
               validate={{
-                required: { value: true, message: 'Se requiere que ingrese su nombre.' },
-                minLength: { value: 1, message: 'Se requiere que su nombre tenga por lo menos 1 caracter' },
-                maxLength: { value: 50, message: 'Su nombre no puede tener más de 50 caracteres' },
+                maxLength: {value: 50, message: 'Su nombre no puede tener más de 50 caracteres'},
               }}
               data-cy="firstname"
             />
             <ValidatedField
               name="lastName"
-              label="Apellidos"
+              label="Apellido 1"
               id="lastName"
-              placeholder="Sus apellidos"
+              placeholder="Su primer apellido"
               validate={{
-                required: { value: true, message: 'Se requiere que ingrese sus apellidos.' },
-                minLength: { value: 1, message: 'Se requiere que sus apellidos tengan por lo menos 1 caracter' },
-                maxLength: { value: 50, message: 'Sus apellidos no pueden tener más de 50 caracteres' },
+                maxLength: {value: 50, message: 'Su apellido no puede tener más de 50 caracteres'},
               }}
-              data-cy="lastname"
+              data-cy="surname1"
+            />
+            <ValidatedField
+              name="lastName"
+              label="Apellido 2"
+              id="lastName"
+              placeholder="Su segundo apellido"
+              validate={{
+                maxLength: {value: 50, message: 'Su apellido no puede tener más de 50 caracteres'},
+              }}
+              data-cy="surname2"
             />
             <ValidatedField
               name="email"
               label="Correo electrónico"
               placeholder="Su correo electrónico"
               type="email"
-              validate={{
-                required: { value: true, message: 'Se requiere un correo electrónico.' },
-                minLength: { value: 5, message: 'Se requiere que su correo electrónico tenga por lo menos 5 caracteres' },
-                maxLength: { value: 254, message: 'Su correo electrónico no puede tener más de 50 caracteres' },
-                validate: v => isEmail(v) || 'Su correo electrónico no es válido.',
-              }}
+              readOnly={true}
               data-cy="email"
             />
             <ValidatedField
@@ -108,26 +110,21 @@ export const SettingsPage = () => {
               label="Imagen de perfil"
               id="image"
               placeholder="Imagen"
-              validate={{
-
-              }}
+              validate={{}}
               type="file"
-              inputProps={{ accept: 'image/*' }}
+              accept={'image/*'}
               data-cy="image"
             />
-            <label >Género</label>
-            <Select className="mt-2 mb-3" options={options} />
+            <label>Género</label>
+            <Select className="mt-2 mb-3" options={options}/>
             <label>País de residencia</label>
-            <Select className="mt-2 mb-3" options={optionsCountry} />
+            <Select className="mt-2 mb-3" options={optionsCountry}/>
 
             <ValidatedField
               name="ubication"
-              label="Ubicacion"
-              id="ubicación"
+              label="Ubicación"
+              id="ubication"
               placeholder="Lugar de residencia"
-              validate={{
-
-              }}
               data-cy="ubication"
             />
 
@@ -137,13 +134,13 @@ export const SettingsPage = () => {
               id="description"
               placeholder="Descripción"
               validate={{
-
+                maxLength: {value: 2500, message: 'Su apellido no puede tener más de 2500 caracteres'},
               }}
               type="textarea"
               data-cy="description"
             />
 
-            <Button  color="primary" type="submit" data-cy="submit">
+            <Button color="primary" type="submit" data-cy="submit">
               Guardar
             </Button>
 
