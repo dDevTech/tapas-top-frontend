f3a4abdc834be38479838c448a54f9830fe7121d
DDev Tech <diego1.berrocal@gmail.com>
2023-04-16 11:53:22 +0200

    Funcionamiento buscador y aparece la lista acutalizada con las tapas coincidentes. Animacion de buscar incluida

--- src/main/webapp/app/shared/reducers/tapa.reducer.ts
+++ src/main/webapp/app/shared/reducers/tapa.reducer.ts
@@ -10,6 +10,7 @@ const initialState = {
   updateSuccess: false,
   favorites: [] as ITapa[],
   last: [] as ITapa[],
+  searchCoincidence: [] as ITapa[],
 };
 
 const apiUrl = 'api/tapas';
@@ -23,6 +24,15 @@ export const getFavorites = createAsyncThunk(
   { serializeError: serializeAxiosError }
 );
 
+export const getSearchCoincidences = createAsyncThunk(
+  'tapas_search_coincidences',
+  async (search: string) => {
+    const requestUrl = `api/tapa/name/${search}`;
+    return axios.get<ITapa[]>(requestUrl);
+  },
+  { serializeError: serializeAxiosError }
+);
+
 export const getLast = createAsyncThunk(
   'tapas_fetch_last',
   async (login: string) => {
@@ -31,41 +41,6 @@ export const getLast = createAsyncThunk(
   },
   { serializeError: serializeAxiosError }
 );
-/*export const getQuestionaryStructuresByCampaignPhase = createAsyncThunk(
-  'questionary-structures/fetch_questionary-structures',
-  async (campaignPhaseId: number) => {
-    const requestUrl = `${apiUrl}/${campaignPhaseId}`;
-    return axios.get<any[]>(requestUrl);
-  },
-  {serializeError: serializeAxiosError}
-);
-
-export const getQuestionaryLevelsByCampaignPhase = createAsyncThunk(
-  'questionary-structures/fetch_questionary-levels',
-  async (campaignPhaseId: number) => {
-    const requestUrl = `api/questionary-levels/${campaignPhaseId}`;
-    return axios.get<IQuestionaryLevel[]>(requestUrl);
-  },
-  {serializeError: serializeAxiosError}
-);
-
-export const getQuestions = createAsyncThunk(
-  'questionary-structures/fetch_questions',
-  async () => {
-    const requestUrl = `api/questions`;
-    return axios.get<any[]>(requestUrl);
-  },
-  {serializeError: serializeAxiosError}
-);
-
-export const saveQuestionaryStructure = createAsyncThunk(
-  'questionary-structures/save-all',
-  async (entities:any, thunkAPI) => {
-    return await axios.post<any[]>('api/questionary-structures', entities);
-  },
-  {serializeError: serializeAxiosError}
-);*/
-
 export const TapaSlice = createSlice({
   name: 'tapas',
   initialState,
@@ -80,6 +55,10 @@ export const TapaSlice = createSlice({
         state.favorites = action.payload.data;
         state.loading = false;
       })
+      .addCase(getSearchCoincidences.fulfilled, (state, action) => {
+        state.searchCoincidence = action.payload.data;
+        state.loading = false;
+      })
       .addCase(getLast.fulfilled, (state, action) => {
         state.last = action.payload.data;
         state.loading = false;
@@ -88,32 +67,14 @@ export const TapaSlice = createSlice({
         state.errorMessage = null;
         state.loading = true;
       })
-      .addMatcher(isPending(getLast), state => {
+      .addMatcher(isPending(getSearchCoincidences), state => {
+        state.errorMessage = null;
+        state.loading = true;
+      })
+      .addMatcher(isPending(getLast), (state, action) => {
         state.errorMessage = null;
         state.loading = true;
       });
-    /*.addCase(getQuestionaryStructuresByCampaignPhase.fulfilled, (state, action) => {
-        state.entities = action.payload.data;
-        state.loadedStructures = true;
-      })
-      .addCase(getQuestionaryLevelsByCampaignPhase.fulfilled, (state, action) => {
-        state.levels = action.payload.data;
-        state.loadedLevels = true;
-      })
-      .addCase(getQuestions.fulfilled, (state, action) => {
-        state.questions = action.payload.data;
-      })
-      .addCase(saveQuestionaryStructure.fulfilled, (state, action) => {
-        state.loadingSave = false;
-      })
-      .addMatcher(isPending(getQuestionaryStructuresByCampaignPhase, getQuestionaryLevelsByCampaignPhase), state => {
-        state.errorMessage = null;
-        state.loadedStructures = false;
-        state.loadedLevels = false;
-      })
-      .addMatcher(isPending(saveQuestionaryStructure), state => {
-        state.loadingSave = true;
-      })*/
   },
 });
 
