7fe38e1718d2cc969935ca2923429009fbc73253
DDev Tech <diego1.berrocal@gmail.com>
2023-04-16 12:18:58 +0200

    Bug fix header degustaciones

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -29,9 +29,9 @@ export const Home = () => (
 export const Tasting = () => (
   // TODO: introducir enlace real cuando se cree la página
   <NavItem>
-    <NavLink tag={Link} to="/tasting" className="d-flex align-items-center">
+    <NavLink tag={Link} to="/" className="d-flex align-items-center">
       <FontAwesomeIcon icon="utensils" />
-      <span>&nbsp;Degustaciones</span>
+      <span>&nbsp;Mis degustaciones</span>
     </NavLink>
   </NavItem>
 );
